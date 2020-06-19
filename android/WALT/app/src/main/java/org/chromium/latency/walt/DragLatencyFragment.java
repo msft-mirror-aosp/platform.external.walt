53e09534845b39a0810279eb8df8b1d89fc31666
Tai Kuo <taikuo@google.com>
2020-06-19 16:56:04 +0800

    walt: app: migrate to AndroidX, AppCompat and new API
    Migrate since api deprecated at sdk 29.
    
    Bug: 149721303
    Test: ./gradlew build --stacktrace --warning-mode all
    Change-Id: Ic4ffacd87ad74db5322e600c4ae60de235a79626
    Signed-off-by: Tai Kuo <taikuo@google.com>

--- android/WALT/app/src/main/java/org/chromium/latency/walt/DragLatencyFragment.java
+++ android/WALT/app/src/main/java/org/chromium/latency/walt/DragLatencyFragment.java
@@ -21,13 +21,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
 
 import com.github.mikephil.charting.charts.ScatterChart;
 import com.github.mikephil.charting.components.Description;
